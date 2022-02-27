0b8b8e58b146f57b7015085d8b9745df8297b122
shiramashiro <zhengrenfu@outlook.com>
2022-02-27 17:48:44 +0800

    初始化Spring Boot项目

---
+++ src/main/java/com/wizardry/qingyou_serve/QingyouServeApplication.java
@@ -0,0 +1,13 @@
+package com.wizardry.qingyou_serve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QingyouServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QingyouServeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wizardry/qingyou_serve/QingyouServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizardry.qingyou_serve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QingyouServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
