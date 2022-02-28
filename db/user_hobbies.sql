b0d05baccd002c6db72f1676c87373194fcf72e8
璨 澜 <>
2022-02-28 07:23:48 +0000

    2.28

--- db/user_hobbies.sql
+++ db/user_hobbies.sql
@@ -1,6 +1,6 @@
 create table user_hobbies(
 id int(20) auto_increment primary key comment "序号",
-hobby_name varchar(20),
-user_id int(20),
+hobby_name varchar(20) comment "爱好类型" ,
+user_id int(20) comment "用户名",
 constraint kf_user_hobbies_user_id foreign key(user_id) references users(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
