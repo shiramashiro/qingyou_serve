2d789ac75c89517d43bb0c7184149474765c2b14
璨 澜 <>
2022-02-28 07:24:16 +0000

    2.28

--- db/user_labels.sql
+++ db/user_labels.sql
@@ -1,6 +1,6 @@
-create table user_label(
-    id int(20) primary key,
-    label_name varchar(10) unique,
-    user_id int(20),
-    constraint kf_user_label_user_id foreign key(user_id) references users(id)
-);
+create table user_labels(
+id int(20) auto_increment primary key comment "序号",
+label_name varchar(10) comment "标签名",
+user_id int(20) comment "用户id",
+constraint kf_user_label_user_id foreign key(user_id) references users(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
