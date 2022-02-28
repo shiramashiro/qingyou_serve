d46fe4b75b4375df59764664a7f7fc81c344885c
璨 澜 <>
2022-02-28 07:25:01 +0000

    2.28

--- db/users.sql
+++ db/users.sql
@@ -1,16 +1,16 @@
 create table users(
-    id int(20) primary key,
-    password varchar(20) unique,
-    uname varchar(15) unique,
-    phone int(14) unique,
-    email varchar(30) unique,
-    created_date datetime,
-    birthday datetime,
-    signature varchar(30),
-    sex varchar(5),
-    location varchar(30),
-    constellation varchar(8),
-    age int(5),
-    avatar varchar(20),
-    occupation varchar(10)
-);
+id int(20) auto_increment primary key comment "序号",
+psw varchar(20) unique  comment "密码",
+uname varchar(15) unique comment "用户名",
+phone varchar(11)  unique comment "手机号",
+email varchar(30) unique comment "邮箱",
+created_date datetime comment "创建日期",
+birthday datetime comment "生日",
+signature varchar(30) comment "个性签名",
+sex varchar(3) comment "性别" ,
+location varchar(30) comment "地区",
+constellation varchar(3) comment "星座",
+age int(3) comment "年龄",
+avatar text comment "URL坐标",
+occupation varchar(10) comment "职业"
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
