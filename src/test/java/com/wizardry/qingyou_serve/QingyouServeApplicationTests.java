38ce249e4d4020764638b040a409aee223f1c7df
璨 澜 <>
2022-03-02 01:22:59 +0000

    数据库连接池配置

--- src/test/java/com/wizardry/qingyou_serve/QingyouServeApplicationTests.java
+++ src/test/java/com/wizardry/qingyou_serve/QingyouServeApplicationTests.java
@@ -1,13 +1,20 @@
 package com.wizardry.qingyou_serve;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class QingyouServeApplicationTests {
 
+    @Autowired  //自动装配
+    private DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        System.out.println(dataSource.getConnection());
     }
 
 }
