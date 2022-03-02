4f969964e9f3daf65f73665ca412c83868a03db5
璨 澜 <>
2022-03-02 00:41:21 +0000

    极小概率宕机异常，插入时异常

---
+++ src/main/java/com/wizardry/qingyou_serve/service/ex/InsertException.java
@@ -0,0 +1,23 @@
+package com.wizardry.qingyou_serve.ex;
+/* 服务器数据库宕机所产生的异常--插入异常类 */
+public class InsertException extends ServiceException{
+    public InsertException() {
+        super();
+    }
+
+    public InsertException(String message) {
+        super(message);
+    }
+
+    public InsertException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InsertException(Throwable cause) {
+        super(cause);
+    }
+
+    protected InsertException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
