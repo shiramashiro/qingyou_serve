87f68d579a3e9d90b92e7ca7ed69bba4d7e1a3f0
shiramashiro <zhengrenfu@outlook.com>
2022-03-07 22:35:55 +0800

    完善Redis案例

--- src/main/java/com/wizardry/qingyou/controller/RedisTestController.java
+++ src/main/java/com/wizardry/qingyou/controller/RedisTestController.java
@@ -13,13 +13,42 @@ public class RedisTestController {
     @Autowired
     private RedisTemplate<String, String> template;
 
-    @RequestMapping(value = "redis/get/{key}", method = RequestMethod.GET)
+    /**
+     * 查询 key 所对应的 value
+     * @param key
+     * @return
+     */
+    @RequestMapping(value = "redis/select/key/{key}", method = RequestMethod.GET)
     public String getRedis(@PathVariable String key) {
         return template.opsForValue().get(key);
     }
 
-    @RequestMapping(value = "redis/set/key/{key}/value/{value}", method = RequestMethod.GET)
+    /**
+     * 插入 key
+     * @param key
+     * @param value
+     */
+    @RequestMapping(value = "redis/insert/key/{key}/value/{value}", method = RequestMethod.GET)
     public void setRedis(@PathVariable String key, @PathVariable String value) {
+        template.opsForValue().set(key, value);
+    }
+
+    /**
+     * 删除 key 所对应的 value
+     * @param key
+     */
+    @RequestMapping(value = "redis/delete/key/{key}", method = RequestMethod.GET)
+    public void removeRedis(@PathVariable String key) {
+        template.opsForValue().getAndDelete(key);
+    }
+
+    /**
+     * 修改 key 所对应的 value
+     * @param key
+     * @param value
+     */
+    @RequestMapping(value = "redis/update/key/{key}/value/{value}", method = RequestMethod.GET)
+    public void updateRedis(@PathVariable String key, @PathVariable String value) {
         template.opsForValue().set(key, value);
     }
 
